df78873525b1b29b6fb0996280b605b6807564d2
Juan Villacorta <juanvillacortac@gmail.com>
2022-10-26 15:16:53 -0400

    esm.sh to skypack for windicss

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export * from "https://esm.sh/windicss@3.5.6/lib";
-export * from "https://esm.sh/windicss@3.5.6/utils/parser";
-export type { Config } from "https://esm.sh/windicss@3.5.6/types/interfaces.d.ts";
+export * from "https://cdn.skypack.dev/windicss@3.5.6/lib?dts";
+export * from "https://cdn.skypack.dev/windicss@3.5.6/utils/parser?dts";
+export type { Config } from "https://cdn.skypack.dev/-/windicss@v3.5.6-Rkw5kkBQFCts4mKv5cS5/dist=es2019,mode=types/types/interfaces.d.ts";
 export type { Plugin } from "https://deno.land/x/fresh@1.1.2/server.ts";
