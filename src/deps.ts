5994f344a3b080e5b45a5cb841325bcdce18a7d8
Juan Villacorta <juanvillacortac@gmail.com>
2022-10-26 13:56:25 -0400

    ../..

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,4 @@
 export * from "https://esm.sh/windicss@3.5.6/lib";
-export * from "https://esm.sh/windicss@3.5.6/utils/style";
 export * from "https://esm.sh/windicss@3.5.6/utils/parser";
 export type { Config } from "https://esm.sh/windicss@3.5.6/types/interfaces.d.ts";
 export type { Plugin } from "https://deno.land/x/fresh@1.1.2/server.ts";
